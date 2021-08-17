40aa84859c9cf72f98112c11c25d632f0cbe049e
Dragos <ginjoveanu.dragos@gmail.com>
2021-08-17 16:23:50 +0300

    changed variable names and declaration types

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-var numarTask = 1;
+var taskNumber = 1;
+
 //adds the given task
 function addTask() {
-  var task = $("#task").val();
-  var isUrgent = $('#urgent').is(':checked');
-  var divClass = "card";
+  const task = $("#task").val();
+  const isUrgent = $('#urgent').is(':checked');
+  let divClass = "card";
   if (isUrgent) {
     divClass += " alert-danger";
   }
+  console.log(isUrgent);
   if (task.length !== 0) {
     $('#list').append(`
-      <div class="` + divClass + `" id = "` + numarTask + `">
+      <div class="` + divClass + `" id = "` + taskNumber + `">
         <div class="card-body">
           <h5>` + task + `</h5>
         </div>
         <div class="card-footer text-muted">
-          <a href="#" class="btn btn-secondary" onclick = "return deleteTask(` + numarTask + `);"><i class="las la-trash-alt"></i></a>
+          <a href="#" class="btn btn-secondary" onclick = "return deleteTask(` + taskNumber + `);"><i class="las la-trash-alt"></i></a>
         </div>
       </div>`
     );
@@ -23,12 +25,12 @@ function addTask() {
     document.getElementById("status").innerHTML = "You forgot to write the task!";
     document.getElementById("status").style.color = "red";
   }
-  ++numarTask;
+  ++taskNumber;
   return false;
 }
+
 //deletes the clicked task
-function deleteTask(idTask) {
-  $('#' + idTask).remove();
-  //congrats, you deleted the rules :)
+function deleteTask(taskId) {
+  $('#' + taskId).remove();
   return false;
 }
